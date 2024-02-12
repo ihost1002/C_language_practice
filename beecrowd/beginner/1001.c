df185ff64e09bc1ae6ae297696f4a6d9b20d442b
ihost1002 <ihost1002@gmail.com>
2024-02-13 01:42:14 +0800

    practice from beecrowd
    	solved 1001, 1002

---
+++ beecrowd/beginner/1001.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(void) {
+	int n1, n2;
+	scanf("%d%d", &n1, &n2);
+	printf("X = %d\n", n1+n2);
+	return 0;
+}
---
+++ beecrowd/beginner/1002.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(void) {
+	double n = 3.14159, R;
+	scanf("%lf", &R);
+	printf("A=%.4f\n", n*R*R);
+	return 0;
+}
